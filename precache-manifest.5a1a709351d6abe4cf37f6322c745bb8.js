d12c4fa809e22c2667fde7a4b692179067fe0510
Antonio Reynoso <105882770+areyno04@users.noreply.github.com>
2022-09-10 11:27:17 -0600

    Updates

---
+++ precache-manifest.5a1a709351d6abe4cf37f6322c745bb8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "641fd3e9c4959785ed2f",
+    "url": "/portfolio-website/static/css/main.41e65e45.chunk.css"
+  },
+  {
+    "revision": "641fd3e9c4959785ed2f",
+    "url": "/portfolio-website/static/js/main.641fd3e9.chunk.js"
+  },
+  {
+    "revision": "45542252074f955224a1",
+    "url": "/portfolio-website/static/js/1.45542252.chunk.js"
+  },
+  {
+    "revision": "2b256896c0c48cf1d86c",
+    "url": "/portfolio-website/static/js/runtime~main.2b256896.js"
+  },
+  {
+    "revision": "74600bfba6300bf216d9206b75e8304f",
+    "url": "/portfolio-website/static/media/comingsoon.74600bfb.jpg"
+  },
+  {
+    "revision": "9ddce8e852ce96625f4e5c86163ab072",
+    "url": "/portfolio-website/static/media/Software Developer Resume.9ddce8e8.pdf"
+  },
+  {
+    "revision": "154a574ea9750c765bf97fadbe550efb",
+    "url": "/portfolio-website/static/media/running.154a574e.jpg"
+  },
+  {
+    "revision": "f95b8a9ba4560c721406f12623507928",
+    "url": "/portfolio-website/static/media/atoms.f95b8a9b.jpg"
+  },
+  {
+    "revision": "5cd4ca072cbc33a4c40df8424d364d50",
+    "url": "/portfolio-website/index.html"
+  }
+];
